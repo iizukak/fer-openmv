e9e19fa0a220ad1738d76128a6372b203c3954af
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2020-03-29 13:04:05 +0900

    Change training epoch

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 # Training Parameters
-EPOCH = 300
+EPOCH = 100
 BATCH_SIZE = 64
 
 # Path  for fer2013.csv
 DATASET_PATH = "./dataset/fer2013.csv"
 
-NETWORK = "resnet_50"
-# NETWORK = "fer_small"
+# NETWORK = "resnet_50"
+NETWORK = "fer_small"
